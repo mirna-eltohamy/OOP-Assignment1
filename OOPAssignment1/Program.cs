27a2ca62b11cb6f9efa8cf70466c61b180e5c56b
Mirna El Tohamy <m.tohamy.2000@gmail.com>
2026-02-17 23:46:02 +0200

    p1q1

--- OOPAssignment1/Program.cs
+++ OOPAssignment1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+#region p1q1
+//namespace OOPAssignment1
+//{
+//    //Part 1
+//    //class PersonClass
+//    //{
+//    //    public string Name;
+//    //}
+
+//    //struct PersonStruct
+//    //{
+//    //    public string Name;
+//    //}
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //PersonClass p1 = new PersonClass();
+//            //p1.Name = "Mirna";
+
+//            //PersonClass p2 = p1;  // copies reference
+//            //p2.Name = "Sara";
+
+//            //Console.WriteLine(p1.Name);  //Sara - changed
+
+
+//            //PersonStruct s1;
+//            //s1.Name = "Mirna";
+
+//            //PersonStruct s2 = s1; // copies value
+//            //s2.Name = "Sara";
+
+//            //Console.WriteLine(s1.Name);  //Mirna - not changed
+
+//        }
+//    }
+//}
+#endregion
